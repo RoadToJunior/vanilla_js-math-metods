cad90d3727906055b806723503ad1b28cf1732fc
NowWeAre-Free <kontodarm@wp.pl>
2023-07-06 19:50:57 +0200

    add 2nd way to display txt after click

--- main.js
+++ main.js
@@ -25,7 +25,12 @@ const showMessage = () => {
   const numberName = Math.floor(Math.random() * names.length);
   const numberPrefix = Math.floor(Math.random() * prefixs.length);
 
-  div.textContent = `${prefixs[numberPrefix]}, że najlepsze imię to ${names[numberName]}`;
+  //   div.textContent = `${prefixs[numberPrefix]}, że najlepsze imię to ${names[numberName]}.`;
+
+  //drugi sposób
+  div.textContent = `${
+    prefixs[Math.floor(Math.random() * prefixs.length)]
+  }, że najlepsze imię to ${names[Math.floor(Math.random() * names.length)]}.`;
 };
 
 btn.addEventListener("click", showMessage);
