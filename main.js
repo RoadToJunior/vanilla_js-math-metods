b4d44f6a57902e3b26fd8e5c3c5132c21ad259c6
NowWeAre-Free <kontodarm@wp.pl>
2023-07-06 19:36:30 +0200

    :tada: init

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector("button");
+const div = document.querySelector("div");
+
+const names = [
+  "Jagienka",
+  "Dobrawa (Dobrusia)",
+  "Daria",
+  "Asia",
+  "Kira",
+  "Marysia",
+  "Anastazja",
+  "Kasia",
+];
+
+const prefixs = [
+  "Wydaje mi się",
+  "Mam wrażenie",
+  "Szczerze powiedziawszy, myślę",
+  "Szczerze uważam",
+  "Na 100% twierdzę",
+  "Jestem pewien",
+];
